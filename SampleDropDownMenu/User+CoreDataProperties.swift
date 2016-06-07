4f18ce13b5566d68faaa1405a1b4627acb7e1b40
Justin M Platz <justinmplatz@justins-mbp.watson.ibm.com>
2016-06-07 13:09:48 -0400

    Add new files for core Data model

---
+++ SampleDropDownMenu/User+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  User+CoreDataProperties.swift
+//  SampleDropDownMenu
+//
+//  Created by Justin M Platz on 6/7/16.
+//  Copyright © 2016 Justin M Platz. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension User {
+
+    @NSManaged var name: String?
+
+}
---
+++ SampleDropDownMenu/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  SampleDropDownMenu
+//
+//  Created by Justin M Platz on 6/7/16.
+//  Copyright © 2016 Justin M Platz. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class User: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
